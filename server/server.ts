fb226230658eb549c4a219300fff14845ffb1dba
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-07-01 11:52:04 +0530

    adds routing and serving static files.

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,4 @@
+const version = "v1";
 const server = Bun.serve({
 	port: 6969,
 	fetch(request) {
@@ -5,11 +6,14 @@ const server = Bun.serve({
 		switch (url.pathname) {
 			case "/login":
 				return login(request);
-			default:
+			case `/${version}`:
 				return new Response(JSON.stringify({ message: "Hello normal" }), {
 					headers: { "Content-Type": "application/json" },
 					status: 200,
 				});
+			default:
+				const BASE_PATH = "./public";
+				return serveFile(BASE_PATH, request);
 		}
 	},
 });
@@ -26,6 +30,12 @@ function login(req: Request): Response {
 			status: 400,
 		});
 	}
+}
+
+function serveFile(BASE_PATH: string, req: Request): Response {
+	const filePath = BASE_PATH + new URL(req.url).pathname;
+	const file = Bun.file(filePath);
+	return new Response(file);
 }
 
 console.log(`Listening on http://localhost:${server.port}`);
