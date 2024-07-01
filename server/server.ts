817867491257de92f41b63eda9e9b1e949daff32
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-07-01 11:40:28 +0530

    decouples code and serves login.

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,31 @@
 const server = Bun.serve({
 	port: 6969,
 	fetch(request) {
-		if (request.method === "GET") {
-			return new Response(JSON.stringify({ message: "Hello!" }), {
-				headers: { "Content-Type": "application/json" },
-			});
+		const url = new URL(request.url);
+		switch (url.pathname) {
+			case "/login":
+				return login(request);
+			default:
+				return new Response(JSON.stringify({ message: "Hello normal" }), {
+					headers: { "Content-Type": "application/json" },
+					status: 200,
+				});
 		}
-		return new Response(JSON.stringify({ message: `Method not allowed` }), {
-			headers: { "Content-Type": "application/json" },
-		});
 	},
 });
+
+function login(req: Request): Response {
+	if (req.method === "GET") {
+		return new Response(JSON.stringify({ message: "Hello login" }), {
+			headers: { "Content-Type": "application/json" },
+			status: 200,
+		});
+	} else {
+		return new Response(JSON.stringify({ message: `Method not allowed` }), {
+			headers: { "Content-Type": "application/json" },
+			status: 400,
+		});
+	}
+}
 
 console.log(`Listening on http://localhost:${server.port}`);
