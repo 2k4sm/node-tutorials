9c12567c71be3c30ecb66ae86707b3d3afef0c5f
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-07-03 10:15:48 +0530

    adds post and put request handling.

--- express_1/server.ts
+++ express_1/server.ts
@@ -3,6 +3,7 @@ import express, { type Request, type Response } from "express";
 const app = express();
 const port = 6969;
 
+app.use(express.json());
 interface Course {
 	id: number;
 	name: string;
@@ -14,10 +15,30 @@ let courses: Course[] = [
 	{ id: 3, name: "course3" },
 	{ id: 4, name: "course4" },
 ];
+
 app.get("/courses", (req: Request, res: Response) => {
 	res.json(courses);
 });
 
+app.post("/courses", (req: Request, res: Response) => {
+	let course: Course = req.body;
+	courses.push(course);
+	res.json(courses);
+});
+
+app.put("/courses/:id", (req: Request, res: Response) => {
+	const courseId = parseInt(req.params.id);
+	const newCourseName = req.body.name;
+	const course = courses.find((course) => course.id === courseId);
+
+	if (course) {
+		course.name = newCourseName;
+		res.json(course);
+	} else {
+		res.status(404).send("Course not found");
+	}
+});
+
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on http://localhost:${port}/`);
 });
